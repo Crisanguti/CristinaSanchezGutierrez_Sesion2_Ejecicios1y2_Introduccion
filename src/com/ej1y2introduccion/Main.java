4e170765d7fb8c1ef0b8295da80ba518ccdc2c31
Cristina <crisanguti@gmail.com>
2022-02-23 17:23:20 +0100

    Initial commit

---
+++ src/com/ej1y2introduccion/Main.java
@@ -0,0 +1,24 @@
+package com.ej1y2introduccion;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int numero1;
+
+        numero1 = 1;
+
+        System.out.println(numero1);
+
+        numero1= 2;
+
+        System.out.println(numero1);
+
+        Integer [] arrayNumeros = {1, 2, 3, 4};
+
+        System.out.println(arrayNumeros[0]);
+
+
+
+    }
+}
